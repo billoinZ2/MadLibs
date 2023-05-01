69179e8a1f67da015ea066bae0f6301405ab8b09
896q <896q@fresnoU.org>
2023-05-01 12:47:47 -0700

    node

---
+++ ComboMenu.js
@@ -0,0 +1,59 @@
+total= 0
+sandwich = Input("what type of sandwich would you like? choose (Chicken, beef, tofu,)
+print=sandwich
+
+if sandwich == "chicken":
+total =5.25
+
+if sandwich == "Beef":
+total = 6.25
+
+if sandwich == "Tofu":
+total = 5.75
+
+print=total
+
+beverage = input("Would you like a drink?")
+if beverage== "yes or No":
+print=beverage
+
+if beverage== "small":
+total= 1.00
+
+if beverage== "medium":
+total= 1.75
+
+if beverage== "large":
+total= 2.25
+
+print=total
+
+
+fries = input("Do you want fries?")
+print=fries
+if fries == "Yes or no":
+
+if fries == "small":
+total= 1.00
+
+if fries == "medium":
+total= 1.50
+
+if fries == "Large":
+total= 2.00
+
+Want =input("Small fries would you like to switch to mega fries")
+
+if fries =="yes or no":
+print=total
+
+ketchup = input("Do you want ketchup packet? ")
+print=ketchup
+if ketchup=="one per packet":
+total= 0.25
+
+if ketchup=="Menu dollor off":
+print=total
+
+Order=input("will there the order")
+
